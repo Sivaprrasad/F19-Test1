f6681e2eb57f20e87f4eb177a6dd3c99422f3ce6
Sivaprrasad Uppalapati <usivaprasad95@gmail.com>
2019-09-12 12:55:10 -0400

    Added Ishealthy Test Case

---
+++ tests/SnakeTest.java
@@ -0,0 +1,48 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SnakeTest {
+	Snake s1;
+	Snake s2;
+	
+
+	@Before
+	public void setUp() throws Exception {
+		s1 = new Snake("Peter", 10, "coffee");
+		s2 = new Snake("Takis", 80, "vegetables");
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+
+	// SivaPrasad Uppalapati - C0744659
+	@Test
+	public void testIsHealthyFunction() {
+
+		boolean s2FavFood = s2.isHealthy();
+		assertTrue("1", s2FavFood);
+		
+	}
+		
+		
+
+	// SivaPrasad Uppalapati - C0744659
+	@Test
+	public void testFitsInCageFunction() {
+		boolean cageLength = s1.fitsInCage(20);
+		if(getlength<cagelenghth )
+		assertTrue("")
+		
+		
+		
+		
+		
+	}
+
+}
